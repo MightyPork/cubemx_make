5c8643a875777eb5840040cbaf2dc4a3fe5d174a
Ondřej Hruška <ondra@ondrovo.com>
2015-08-24 17:06:15 +0200

    Converted newlines, added newline conversion to update_mf

--- update_mf.sh
+++ update_mf.sh
@@ -15,6 +15,14 @@ echo -e "\e[0;32mPatching Makefile...\e[0m"
 #cp Makefile Makefile.patch-bak
 patch Makefile MFGEN/fix_mf.patch
 
+echo -e "\e[0;32mConverting newlines...\e[0m"
+echo "Src..."
+find Src -type f -print0 | xargs -0 dos2unix -q
+echo "Inc..."
+find Inc -type f -print0 | xargs -0 dos2unix -q
+echo "Drivers..."
+find Drivers -type f -print0 | xargs -0 dos2unix -q
+
 echo -e "\e[0;32mDone.\e[0m"
 
 
